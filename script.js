cfc4feac3dd3d6a9c1413031ef380382032fd586
Leo Allesch <leoallesch@gmail.com>
2021-11-23 09:32:32 -0500

    add,edit,remove working

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function update () {
     window.sessionStorage.clear();
 }
 
-function addRow(type, index) {
-    document.getElementById("add-" + type).innerHTML = "Add " + type.charAt(0).toUpperCase() + type.slice(1);
-    
+function addRow(type,index) {
+    document.getElementById("edit-" + type).style.display = "none";
+    document.getElementById("add-" + type).style.display = "block";  
+
     if (window.sessionStorage.getItem(type) == null){
        var data = [];
     } else {
@@ -30,29 +31,30 @@ function addRow(type, index) {
 
     if (valueType != "" && valueCost != ""){
         if (index != -1){
-            var td = index.parentNode;
-            var tr = td.parentNode;
-            data[tr.rowIndex-1][0] = valueType;
-            data[tr.rowIndex-1][1] = valueCost;
+            data.splice(index-1,1,[valueType,valueCost]);
             window.sessionStorage.setItem(type, JSON.stringify(data));
+            
+            var table = document.querySelector("." + type+ " table");
+            table.rows[index].cells[0].innerHTML = valueType;
+            table.rows[index].cells[1].innerHTML = "$" + valueCost;
         } else {
             data.push([valueType,valueCost]);
             window.sessionStorage.setItem(type, JSON.stringify(data));
-        }        
-
-        var table = document.querySelector("." + type+ " table");
-        var row = table.insertRow(index);
-        var cellType = row.insertCell(0);
-        var cellCost = row.insertCell(1);
-        var edit = row.insertCell(2);
-        var del = row.insertCell(3);
-    
-        cellType.innerHTML = valueType;
-        cellCost.innerHTML = "$" + valueCost;
-        edit.innerHTML = "<button id='delete' type='button'>Edit</button>";
-        edit.addEventListener("click", function(){editRow(type, edit.firstChild)});
-        del.innerHTML = "<button id='delete' type='button'>Delete</button>";
-        del.addEventListener("click", function(){removeRow(type, del.firstChild)});
+            
+            var table = document.querySelector("." + type+ " table");
+            var row = table.insertRow(index);
+            var cellType = row.insertCell(0);
+            var cellCost = row.insertCell(1);
+            var edit = row.insertCell(2);
+            var del = row.insertCell(3);
+        
+            cellType.innerHTML = valueType;
+            cellCost.innerHTML = "$" + valueCost;
+            edit.innerHTML = "<button id='delete' type='button'>Edit</button>";
+            edit.addEventListener("click", function(){editRow(type, edit.firstChild)});
+            del.innerHTML = "<button id='delete' type='button'>Delete</button>";
+            del.addEventListener("click", function(){removeRow(type, del.firstChild)});
+        }     
     }
 }
 
@@ -68,16 +70,27 @@ function removeRow(type, btn){
 }
 
 function editRow(type,btn){
-	var data = JSON.parse(window.sessionStorage.getItem(type));
+    var data = JSON.parse(window.sessionStorage.getItem(type));
 
     var td = btn.parentNode;
-    var tr = btn.parentNode;
+    var tr = td.parentNode;
+    index = tr.rowIndex;
 	
-	document.getElementById(type + "-type").value = data[tr.rowIndex-1][0];
-	document.getElementById(type + "-cost").value = data[tr.rowIndex-1][1];
+    if (type == "savings" && data[index-1][0] != "Emergency"){
+        
+        document.getElementById("hidden-savings").style.display = "block";
+        document.getElementById(type + "-type").value = "custom";
+        document.getElementById(type + "-custom").value = data[index-1][0];
+        document.getElementById(type + "-cost").value = data[index-1][1];
+    } else {
+        document.getElementById(type + "-type").value = data[index-1][0];
+	    document.getElementById(type + "-cost").value = data[index-1][1];
+    }
 
-	document.getElementById("add-" + type).innerHTML = "Save";
+	document.getElementById("edit-" + type).style.display = "block";
+    document.getElementById("add-" + type).style.display = "none";
 
+    document.getElementById("edit-" + type).addEventListener("click",function(){addRow(type,index)});
 }
 
 function submit() {
