5799657fb39495619e80025da35bb3bc11fd96de
Leo Allesch <leoallesch@gmail.com>
2021-12-16 13:27:47 -0500

    work on homepage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+window.addEventListener("DOMContentLoaded", function(){update()});
+
+function hasBudget(){
+    if (window.localStorage.getItem("budget") != null)
+        return true;
+    return false;
+}
+
+function getData(){
+    return JSON.parse(window.localStorage.getItem("budget"));
+}
+
+function update(){
+    if(hasBudget()){
+        var x = document.getElementById("create");
+        x.classList.add("hidden");
+    } else {
+        var x = document.getElementById("overview");
+        x.classList.add("hidden");
+    }
+}
+
+
+//css functions
+
 function showDiv(divId){
     var x = document.getElementById(divId);
     x.classList.toggle("hidden");
